3d3d863b178983e41c3480bbb8565feffd278987
Azmarul Islam <imjohnny.dev@gmail.com>
2023-09-09 01:28:16 +0600

    Assignment Module 5

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,7 @@ class MyApp extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "Live Test Module 5",
+      title: "Assignment Module 5 ",
       debugShowCheckedModeBanner: true,
       home: homePage(),
     );
@@ -16,106 +16,45 @@ class MyApp extends StatelessWidget{
 }
 
 class homePage extends StatelessWidget{
-  final List<String> dataList = ['Item 1', 'Item 2', 'Item 3', 'Item 4'];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Johnny'), // App title in the center
-        actions: <Widget>[
-          // Icons aligned to the right
-          IconButton(
-            icon: Icon(Icons.abc), // Icon for the first action
-            onPressed: () {
-              // Add your onPressed logic here
-            },
-          ),
-          IconButton(
-            icon: Icon(Icons.do_not_disturb_on_total_silence), // Icon for the second action
-            onPressed: () {
-              // Add your onPressed logic here
-            },
+        title: Text("Home"),
+        centerTitle: true,
+        backgroundColor: Colors.green,
+        toolbarHeight: 100,
+        elevation: 70,
+        leading: Icon(
+          Icons.store_mall_directory_sharp,
+        ),
+        actions: [
+          Padding(
+            padding: EdgeInsets.only(right: 10),
+            child: Icon(Icons.search),
           ),
         ],
       ),
-      //     body: Column(
-      //       children: [
-      //         Text(
-      // "Home Page",
-      //           style: TextStyle(
-      //             color: Colors.cyan,
-      //             fontSize: 18,
-      //           ),
-      //         ),
-      //       ],
-      //     ),
       body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            // Add your button click logic here
-          },
-          style: ElevatedButton.styleFrom(
-            primary: Colors.blue, // Background color
-            onPrimary: Colors.white, // Text color
-            padding: EdgeInsets.all(20), // Padding around the button
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(10), // Rounded corners
-            ),
-            textStyle: TextStyle(
-              fontSize: 20, // Text size
-            ),
-            shadowColor: Colors.grey, // Shadow color
-            elevation: 5, // Elevation (shadow depth)
-          ),
-          child: Text('Stylish Button'),
-        ),
-      ),
-
-      drawer: Drawer(
-        child: ListView(
-          padding: EdgeInsets.zero,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            UserAccountsDrawerHeader(
-                accountName: Text("John Doe"),
-                accountEmail: Text("johndoe@example.com"),
-                currentAccountPicture: CircleAvatar(
-                  backgroundImage: NetworkImage("https://pbs.twimg.com/profile_images/1336424559189270528/hs4a6Zit_400x400.jpg"),
-                )
-            ),
-            ListTile(
-              leading: Icon(Icons.home),
-              title: Text("Home"),
-              onTap: () {
-                // Handle home button tap
-              },
-            ),
-            ListTile(
-              leading: Icon(Icons.settings),
-              title: Text("Settings"),
-              onTap: () {
-                // Handle settings button tap
-              },
-            ),
-          ],
-        ),
-      ),
-      endDrawer: Drawer(
-        child: ListView(
-          padding: EdgeInsets.all(0),
-          children: [
-            UserAccountsDrawerHeader(
-              accountName: Text("Azmarul Islam Johnny"),
-              accountEmail: Text("info@iamazmarul.com"),
-              currentAccountPicture: CircleAvatar(
-                backgroundImage: NetworkImage("https://pbs.twimg.com/profile_images/1336424559189270528/hs4a6Zit_400x400.jpg"),
-              ),
-            ),
-            ListTile(
-              title: Text("Home"),
-              leading: Icon(Icons.ice_skating),
-              onTap: (){
-
-              },
+            Text("This Is Mod 5 Assignment", style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+            ),),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                RichText(text: TextSpan(
+                  children: [
+                    TextSpan(text: 'My ', style: TextStyle(color: Colors.red, fontSize: 25,)),
+                    TextSpan(text: 'phone ', style: TextStyle(color: Colors.blue, fontSize: 15,)),
+                    TextSpan(text: 'name ', style: TextStyle(color: Colors.purpleAccent, fontSize: 20, fontWeight: FontWeight.w700)),
+                    TextSpan(text: 'Your phone name ', style: TextStyle(color: Colors.orange, fontSize: 25,)),
+                  ],
+                )),
+              ],
             )
           ],
         ),
