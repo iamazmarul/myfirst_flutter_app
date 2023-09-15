e4c1384c6d5db54150f5c20b8e9dbb34a1a925ed
Azmarul Islam <imjohnny.dev@gmail.com>
2023-09-16 05:44:28 +0600

    Directory App Clone

--- lib/main.dart
+++ lib/main.dart
@@ -152,36 +152,39 @@ class aboutUs extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Text("About Us"),
+        backgroundColor: Colors.green,
       ),
-      body: Center(
-        child: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.only(top: 20),
-              child: Image.network(
-                "https://upload.wikimedia.org/wikipedia/commons/c/c1/DMCB_Logo_%28PNG_File%29.jpg",
-                height: 70,
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(top: 20),
+                child: Image.network(
+                  "https://upload.wikimedia.org/wikipedia/commons/c/c1/DMCB_Logo_%28PNG_File%29.jpg",
+                  height: 70,
+                ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(10),
-              child: Column(
-                children: [
-                  Text('''The Dhaka Mercantile Co-operative Bank Limited [DMCBL] is a Shariah based co-operative bank in Bangladesh. Dhaka Mercantile Co-operative Bank was established on 6th January, 1973 under the Cooperative Society Act & Rules of Bangladesh. For DMCBL, the words "Society" and "Bank" have become synonymous/ as is the case for DMCBL. In 2001, Group Captain Abu Zafar Chowdhury (Retd.) GD(P) Psc. resurrected the operation with the vision of a poverty-free Bangladesh.
+              Padding(
+                padding: const EdgeInsets.all(10),
+                child: Column(
+                  children: [
+                    Text('''The Dhaka Mercantile Co-operative Bank Limited [DMCBL] is a Shariah based co-operative bank in Bangladesh. Dhaka Mercantile Co-operative Bank was established on 6th January, 1973 under the Cooperative Society Act & Rules of Bangladesh. For DMCBL, the words "Society" and "Bank" have become synonymous/ as is the case for DMCBL. In 2001, Group Captain Abu Zafar Chowdhury (Retd.) GD(P) Psc. resurrected the operation with the vision of a poverty-free Bangladesh.
 
-                  DMCBL is a Specialized Development Co-operative Financial Institution in Bangladesh, offering retail and commercial banking under a co-operative theme. DMCBL provides collateral-free credit, investment to the businesses that are legal entities and their entrepreneurs are able, well-informed, success-motivated, and has good understanding of the business, and has a good, acceptable social reputation within the community.
+                    DMCBL is a Specialized Development Co-operative Financial Institution in Bangladesh, offering retail and commercial banking under a co-operative theme. DMCBL provides collateral-free credit, investment to the businesses that are legal entities and their entrepreneurs are able, well-informed, success-motivated, and has good understanding of the business, and has a good, acceptable social reputation within the community.
 
-                  The Dhaka Mercantile Co-operative Bank Limited is the largest Co-operative under the supervision of the Co-operative Department of Ministry of L.G.R.D Bangladesh. This institution is audited annually by the Co-operative Department of the Government. In addition to a governmental audit, DMCBL internally appoints S.F Ahmed & Company, Chartered Accountants to scrutinize the bank's financial standing/ strength and depth, as well as advice on necessary measures and practices so as to comply with the internationally accepted standards of accounting and book-keeping. Hiring an external company to do internal audits also satisfies significant stake holder and legislative needs and rules.
+                    The Dhaka Mercantile Co-operative Bank Limited is the largest Co-operative under the supervision of the Co-operative Department of Ministry of L.G.R.D Bangladesh. This institution is audited annually by the Co-operative Department of the Government. In addition to a governmental audit, DMCBL internally appoints S.F Ahmed & Company, Chartered Accountants to scrutinize the bank's financial standing/ strength and depth, as well as advice on necessary measures and practices so as to comply with the internationally accepted standards of accounting and book-keeping. Hiring an external company to do internal audits also satisfies significant stake holder and legislative needs and rules.
 
-                  Our per capita annual gross income is EURO 10000 minimum being EURO 2000 in coming years, It will be further enhanced.
+                    Our per capita annual gross income is EURO 10000 minimum being EURO 2000 in coming years, It will be further enhanced.
 
-                  The Dhaka Mercantile Co-operative Bank Limited has over 142 Branches with over 3700 energetic and enthusiastic staff that thrive towards excellent service, sustainable profitability, and an overall socially responsible financial institution.
+                    The Dhaka Mercantile Co-operative Bank Limited has over 142 Branches with over 3700 energetic and enthusiastic staff that thrive towards excellent service, sustainable profitability, and an overall socially responsible financial institution.
 
-                  DMCBL emanates its strength from its highly acclaimed management, strong customer focus and relationships built on integrity, superior service and mutual benefit.''')
-                ],
-              ),
-            )
-          ],
+                    DMCBL emanates its strength from its highly acclaimed management, strong customer focus and relationships built on integrity, superior service and mutual benefit.''')
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
       )
     );
