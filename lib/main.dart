9df2883cb9eaba948ec8d486923f0aa4de88bacc
Azmarul Islam <imjohnny.dev@gmail.com>
2023-09-18 00:05:56 +0600

    Assignment Module 6

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,16 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const Myapp());
+  runApp(Myapp());
 }
 
 class Myapp extends StatelessWidget {
-  const Myapp({super.key});
+  Myapp({super.key});
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: true,
       title: "Module 6 Assignment",
       home: homePage(),
@@ -16,9 +18,17 @@ class Myapp extends StatelessWidget {
   }
 }
 
-
 class homePage extends StatelessWidget {
-  const homePage({super.key});
+  homePage({super.key});
+
+  List<String> dogsImage = [
+    "https://images.pexels.com/photos/1108099/pexels-photo-1108099.jpeg",
+    "https://hips.hearstapps.com/hmg-prod/images/best-guard-dogs-1650302456.jpeg",
+    "https://images.pexels.com/photos/257519/pexels-photo-257519.jpeg",
+    "https://hips.hearstapps.com/hmg-prod/images/best-guard-dogs-1650302456.jpeg",
+    "https://images.pexels.com/photos/257519/pexels-photo-257519.jpeg",
+    "https://images.pexels.com/photos/1108099/pexels-photo-1108099.jpeg",
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -29,14 +39,17 @@ class homePage extends StatelessWidget {
       ),
       body: SingleChildScrollView(
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
             Padding(
-              padding:  EdgeInsets.all(10.0),
-              child: Text("Welcome to My Photo Gallery!",
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                "Welcome to My Photo Gallery!",
                 style: TextStyle(
                   fontSize: 24,
                   fontWeight: FontWeight.bold,
-                ),),
+                ),
+              ),
             ),
             Column(
               children: [
@@ -44,13 +57,124 @@ class homePage extends StatelessWidget {
                   padding: EdgeInsets.all(8.0),
                   child: TextField(
                     decoration: InputDecoration(
-                      hintText: "hsdu",
+                      hintText: "Search for photos...",
                       border: OutlineInputBorder(),
                     ),
                   ),
                 )
               ],
-            )
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                GridView.builder(
+                  shrinkWrap: true,
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 3),
+                  itemCount: dogsImage.length,
+                  itemBuilder: (context, index) {
+                    final dogsImages = dogsImage[index];
+                    return GestureDetector(
+                      onTap: () {},
+                      child: Padding(
+                          padding: EdgeInsets.all(
+                              20), // Adjust the padding as needed
+                          child: Column(
+                            children: [
+                              Image.network(
+                                dogsImages,
+                                height: 90,
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.only(top: 10),
+                                child: Text(
+                                  "Photos $index",
+                                  style: TextStyle(fontWeight: FontWeight.w600),
+                                ),
+                              ),
+                            ],
+                          )),
+                    );
+                  },
+                ),
+              ],
+            ),
+            Column(
+              children: [
+                ListView(
+                  shrinkWrap: true,
+                  children: [
+                    ListTile(
+                      leading: ClipOval(
+                        child: Image.network(
+                          "https://hips.hearstapps.com/hmg-prod/images/best-guard-dogs-1650302456.jpeg",
+                          height: 50,
+                          width: 50,
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                      title: Text("Photo 1"),
+                      subtitle: Text("Image Details"),
+                    ),
+                  ],
+                ),
+                ListView(
+                  shrinkWrap: true,
+                  children: [
+                    ListTile(
+                      leading: ClipOval(
+                        child: Image.network(
+                          "https://images.pexels.com/photos/257519/pexels-photo-257519.jpeg",
+                          height: 50,
+                          width: 50,
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                      title: Text("Photo 1"),
+                      subtitle: Text("Image Details"),
+                    ),
+                  ],
+                ),
+                ListView(
+                  shrinkWrap: true,
+                  children: [
+                    ListTile(
+                      leading: ClipOval(
+                        child: Image.network(
+                          "https://images.pexels.com/photos/1108099/pexels-photo-1108099.jpeg",
+                          height: 50,
+                          width: 50,
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                      title: Text("Photo 1"),
+                      subtitle: Text("Image Details"),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+      Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Container(
+            width: 50, // Width of the circular background
+            height: 50, // Height of the circular background
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              color: Colors.blue, // Background color of the circle
+            ),
+            child: Center(
+              child: IconButton(
+                icon: Icon(Icons.cloud_upload, size: 20, color: Colors.white), // Upload icon
+                onPressed: () {
+
+                },
+              ),
+            ),
+          ),
+        ],
+      ),
           ],
         ),
       ),
