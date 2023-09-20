00dc72a09f806ab636649334f7df221808e6466e
Azmarul Islam <imjohnny.dev@gmail.com>
2023-09-20 22:17:55 +0600

    Assignment Module 6

--- lib/main.dart
+++ lib/main.dart
@@ -1,187 +1,64 @@
-import 'package:flutter/material.dart';
-
-void main() {
-  runApp(Myapp());
-}
-
-class Myapp extends StatelessWidget {
-  Myapp({super.key});
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: true,
-      title: "Module 6 Assignment",
-      home: homePage(),
-    );
-  }
-}
-
-class homePage extends StatelessWidget {
-  homePage({super.key});
-
-  final List dogsImage = [
-    "https://images.pexels.com/photos/1108099/pexels-photo-1108099.jpeg",
-    "https://hips.hearstapps.com/hmg-prod/images/best-guard-dogs-1650302456.jpeg",
-    "https://images.pexels.com/photos/257519/pexels-photo-257519.jpeg",
-    "https://hips.hearstapps.com/hmg-prod/images/best-guard-dogs-1650302456.jpeg",
-    "https://images.pexels.com/photos/257519/pexels-photo-257519.jpeg",
-    "https://images.pexels.com/photos/1108099/pexels-photo-1108099.jpeg",
-  ];
-
-  MySnackbar(message, context) {
-    ScaffoldMessenger.of(context).showSnackBar(
-      SnackBar(
-        content: Text(message),
-      ),
-    );
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Photo Gallery"),
-        centerTitle: true,
-      ),
-      body: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(12),
-              child: Text(
-                "Welcome to My Photo Gallery!",
-                style: TextStyle(
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            ),
-            Column(
-              children: [
-                Padding(
-                  padding: EdgeInsets.all(8.0),
-                  child: TextField(
-                    decoration: InputDecoration(
-                      hintText: "Search for photos...",
-                      border: OutlineInputBorder(),
-                    ),
-                  ),
-                )
-              ],
-            ),
-            Column(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                GridView.builder(
-                  shrinkWrap: true,
-                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                      crossAxisCount: 3),
-                  itemCount: dogsImage.length,
-                  itemBuilder: (context, index) {
-                    final dogsImages = dogsImage[index];
-                    return GestureDetector(
-                      onTap: () {
-                        MySnackbar("Photo $index", context);
-                      },
-                      child: Padding(
-                          padding: EdgeInsets.all(20),
-                          child: Column(
-                            children: [
-                              Image.network(dogsImages, height: 90),
-                              Padding(
-                                padding: const EdgeInsets.only(top: 10),
-                                child: Text("Photos $index",
-                                  style: TextStyle(fontWeight: FontWeight.w600),
-                                ),
-                              ),
-                            ],
-                          )),
-                    );
-                  },
-                ),
-              ],
-            ),
-            Column(
-              children: [
-                ListView(
-                  shrinkWrap: true,
-                  children: [
-                    ListTile(
-                      leading: ClipOval(
-                        child: Image.network(
-                          "https://hips.hearstapps.com/hmg-prod/images/best-guard-dogs-1650302456.jpeg",
-                          height: 50,
-                          width: 50,
-                          fit: BoxFit.cover,
-                        ),
-                      ),
-                      title: Text("Photo 1"),
-                      subtitle: Text("Image Details"),
-                    ),
-                  ],
-                ),
-                ListView(
-                  shrinkWrap: true,
-                  children: [
-                    ListTile(
-                      leading: ClipOval(
-                        child: Image.network(
-                          "https://images.pexels.com/photos/257519/pexels-photo-257519.jpeg",
-                          height: 50,
-                          width: 50,
-                          fit: BoxFit.cover,
-                        ),
-                      ),
-                      title: Text("Photo 1"),
-                      subtitle: Text("Image Details"),
-                    ),
-                  ],
-                ),
-                ListView(
-                  shrinkWrap: true,
-                  children: [
-                    ListTile(
-                      leading: ClipOval(
-                        child: Image.network(
-                          "https://images.pexels.com/photos/1108099/pexels-photo-1108099.jpeg",
-                          height: 50,
-                          width: 50,
-                          fit: BoxFit.cover,
-                        ),
-                      ),
-                      title: Text("Photo 1"),
-                      subtitle: Text("Image Details"),
-                    ),
-                  ],
-                ),
-              ],
-            ),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Container(
-                  width: 50, // Width of the circular background
-                  height: 50, // Height of the circular background
-                  decoration: BoxDecoration(
-                    shape: BoxShape.circle,
-                    color: Colors.blue, // Background color of the circle
-                  ),
-                  child: Center(
-                    child: IconButton(
-                      icon: Icon(Icons.cloud_upload,
-                          size: 20, color: Colors.white), // Upload icon
-                      onPressed: () {
-                        MySnackbar("Photos Uploaded Successfully!", context);
-                      },
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
+// import 'package:flutter/cupertino.dart';
+// import 'package:flutter/material.dart';
+//
+// void main(){
+//   runApp(MyApp());
+// }
+//
+// class MyApp extends StatelessWidget {
+//    MyApp({super.key});
+//
+//    bool _switch; false;
+//    ThemeData _dark = ThemeData(brightness: Brightness.dark);
+//    ThemeData _light = ThemeData(brightness: Brightness.light)
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: "Module 7",
+//       debugShowCheckedModeBanner: false,
+//       home: HomeScreen(),
+//       theme: ThemeData(
+//         _switch ? _dark : _light,
+//         elevatedButtonTheme: ElevatedButtonThemeData(
+//           style: ElevatedButton.styleFrom(
+//             backgroundColor: Colors.red,
+//             foregroundColor: Colors.white,
+//           ),
+//         ),
+//       ),
+//       darkTheme: ThemeData(
+//         brightness: Brightness.dark,
+//         appBarTheme: AppBarTheme(
+//           backgroundColor: Colors.pink,
+//         )
+//       ),
+//       themeMode: ThemeMode.dark,
+//     );
+//   }
+// }
+//
+// class HomeScreen extends StatelessWidget {
+//   HomeScreen({super.key});
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text("Home"),
+//       ),
+//       body: Center(
+//         child: Column(
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           children: [
+//             Switch(value: _switch, onChanged: (_newvalue){
+//             setState((){
+//               _switch = newvalue;
+//             });
+//             })
+//           ],
+//         ),
+//       )
+//     );
+//   }
+// }
