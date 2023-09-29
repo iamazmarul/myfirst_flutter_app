64f7796024fabe7438b76dd55942c39b2be6ee07
iamazmarul <55909678+iamazmarul@users.noreply.github.com>
2023-09-30 00:01:24 +0600

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "Assignment Module 8"
+      title: "Assignment Module 8",
       home: ProfileScreen(),
     );
   }
