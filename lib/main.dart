4b4f0d9ed13e2701507ff79f20c9495610c84eed
Azmarul Islam <imjohnny.dev@gmail.com>
2023-09-29 23:39:23 +0600

    Live Test Module 8

--- lib/main.dart
+++ lib/main.dart
@@ -8,69 +8,60 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: ProfileScreen(),
+      title: "Live Test M 8",
+      home: HomeScreen(),
     );
   }
 }
 
-class ProfileScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
+
     final orientation = MediaQuery.of(context).orientation;
     final isPortrait = orientation == Orientation.portrait;
 
     return Scaffold(
       appBar: AppBar(
-        title: Text('Profile'),
+        title: Text("News Feed"),
       ),
       body: SingleChildScrollView(
-        child: Center(
-          child: isPortrait ? PortraitLayout() : LandscapeLayout(),
-        ),
+        child: isPortrait ?  PortraitLayout() : LandscapeLayout(),
       ),
     );
   }
 }
 
+
 class PortraitLayout extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(left: 8, right: 8),
+    return Expanded(
       child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          SizedBox(height: 20.0),
-          CircleAvatar(
-            radius: MediaQuery.sizeOf(context).width / 2,
-            backgroundImage: NetworkImage(
-                "https://cdn.mos.cms.futurecdn.net/JxJ548FZEJo2SnDQqK6qK7.jpg"), // Replace with your profile image
-          ),
-          SizedBox(height: 20.0),
-          Text(
-            'Md. Azmarul Islam',
-            style: TextStyle(
-              fontSize: 24.0,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-          SizedBox(height: 10.0),
-          Text(
-            "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown",
-          ),
-          SizedBox(height: 20.0),
-          GridView.builder(
+          ListView.builder(
+            physics: BouncingScrollPhysics(),
             shrinkWrap: true,
-            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 3,
-              crossAxisSpacing: 10.0,
-              mainAxisSpacing: 10.0,
-            ),
-            itemCount: 6, // Number of images
-            itemBuilder: (context, index) {
-              return Image.network(
-                  "https://cdn.mos.cms.futurecdn.net/JxJ548FZEJo2SnDQqK6qK7.jpg",
-                  fit: BoxFit.cover);
+            itemCount: 10, // Replace with your desired number of items
+            itemBuilder: (BuildContext context, int index) {
+              return Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  height: 150, // This height might be too much for typical portrait layout
+                  alignment: Alignment.center,
+                  color: Colors.white70,
+                  child: Image.network(
+                    "https://via.placeholder.com/150", width: 150, height: 150,
+                  )
+                ),
+              );
             },
           ),
         ],
@@ -79,64 +70,32 @@ class PortraitLayout extends StatelessWidget {
   }
 }
 
+
 class LandscapeLayout extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // final screenWidth = MediaQuery.of(context).size.width;
-
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.start, // Align items to the top
-        children: [
-          Container(
-            alignment: Alignment.topCenter, // Align the CircleAvatar to the top
-            child: CircleAvatar(
-              radius: MediaQuery.sizeOf(context).height / 2.7,
-              backgroundImage: NetworkImage(
-                  "https://cdn.mos.cms.futurecdn.net/JxJ548FZEJo2SnDQqK6qK7.jpg"),
-            ),
-          ),
-          SizedBox(width: 20),
-          Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  'Md. Azmarul Islam',
-                  style: TextStyle(
-                    fontSize: 24.0,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                SizedBox(height: 10.0),
-                Text(
-                  "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown",
-                  style: TextStyle(
-                    fontSize: 18.0,
-                    color: Colors.grey,
-                  ),
-                ),
-                SizedBox(height: 10.0),
-                GridView.builder(
-                  shrinkWrap: true,
-                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 3,
-                    crossAxisSpacing: 10.0,
-                    mainAxisSpacing: 10.0,
-                  ),
-                  itemCount: 6,
-                  itemBuilder: (context, index) {
-                    return Image.network(
-                        "https://cdn.mos.cms.futurecdn.net/JxJ548FZEJo2SnDQqK6qK7.jpg",
-                        fit: BoxFit.cover);
-                  },
-                ),
-              ],
-            ),
-          ),
-        ],
+    return GridView.builder(
+      physics: BouncingScrollPhysics(),
+      shrinkWrap: true,
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
       ),
+      itemCount: 10,
+      itemBuilder: (context, index) {
+        return Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Container(
+            height: 150,
+            alignment: Alignment.topCenter,
+            color: Colors.white70,
+            child: Image.network(
+              "https://via.placeholder.com/150",
+              width: 150,
+              height: 150,
+            ),
+          ),
+        );
+      },
     );
   }
 }
