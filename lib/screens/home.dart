6e98d3a8ae63870cd50f1833a1126ab508ced0e0
Azmarul Islam <imjohnny.dev@gmail.com>
2023-10-13 22:56:32 +0600

    Live Test Module 10

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -8,11 +8,31 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  List<dynamic> todoList = [];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Assignment"),
+      ),
+      body: Column(
+        children: [
+          Column(
+            children: [
+              TextField(),
+            ],
+          ),
+          ListView.builder(
+              shrinkWrap: true,
+              itemCount: todoList.length,
+              itemBuilder: (context, index) {
+                return Card(
+                  child: ListTile(
+                    title: Text(todoList[index].toString()),
+                  ),
+                );
+              }),
+        ],
       ),
     );
   }
