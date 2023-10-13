544dca290ec835f54083a8e229134a0185c2826e
Azmarul Islam <imjohnny.dev@gmail.com>
2023-10-13 22:14:46 +0600

    Assignment Module 10

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,15 @@
 import 'package:flutter/material.dart';
-import '../screens/home.dart';
+import 'package:myfirst_flutter_app/screens/home.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      title: "Assignment Module 9",
-      debugShowCheckedModeBanner: false,
+    return MaterialApp(
+      title: "Assignment Module 10",
       home: Home(),
     );
   }
