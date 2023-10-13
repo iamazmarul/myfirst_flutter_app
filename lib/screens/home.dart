2ee97570721d30d6ad0d8c8c688f8e18e8e58f49
Azmarul Islam <imjohnny.dev@gmail.com>
2023-10-13 23:30:07 +0600

    Live Test Module 10

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -8,11 +8,47 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  List<dynamic> selectionList = List.filled(8, false);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Assignment"),
+        title: const Text("Selection Screen"),
+      ),
+      body: ListView.builder(
+          itemCount: 8,
+          itemBuilder: (context, index) {
+            return ListTile(
+              title: Text("Item ${index + 1}"),
+              tileColor: selectionList[index] ? Colors.blue : null,
+              onTap: () {
+                setState(() {
+                  selectionList[index] = !selectionList[index];
+                });
+              },
+            );
+          }),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          int selectionCount = selectionList.where((item) => item).length;
+          showDialog(
+              context: context,
+              builder: (BuildContext context) {
+                return AlertDialog(
+                  title: const Text("Selected Items"),
+                  content: Text("Number of selected items: $selectionCount"),
+                  actions: [
+                    TextButton(
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                        child: const Text("Close"))
+                  ],
+                );
+              });
+        },
+        child: const Icon(Icons.check),
       ),
     );
   }
